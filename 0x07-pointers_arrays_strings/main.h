d077b635786a610e25e53aa0ec7c4a5a70311b8e
Moswax <moses.wakuloba@gmail.com>
2023-10-23 03:00:17 -0700

    0-memset.c

---
+++ 0x07-pointers_arrays_strings/main.h
@@ -0,0 +1,15 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+char *_memset(char *S, char b, usigned int n);
+int _putchar(char c);
+char *_memcpy(char *dest, char *src, usigned int n);
+char *_strchr(char *s, char c);
+unsigned int _strspn(char *s, char *accept);
+char *_strpbrk(char *s, char *accept);
+char *_strstr(char *haystack, char *needle);
+void print_chessboard*(char (*a([8]);
+void print_diagdums(int *a, int size);
+void set_string(char **s, char *to);
+
+#endif
